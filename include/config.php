25fa6afb99829204075ac5526071dfbf55f2ad39
yandinovriandi <yandinovriandi@gmail.com>
2021-11-30 06:47:05 +0700

    wip

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['rihana.net'] = array ('1'=>'rihana.net!sg-7.hostddns.us:3465','rihana.net@|@rihanafauziah','rihana.net#|#o5KfmZ2nnJqg','rihana.net%rihana.net','rihana.net^rihana.net','rihana.net&Rp','rihana.net*10','rihana.net(1','rihana.net)','rihana.net=10','rihana.net@!@enable');
